11b9efc89eb781cdd394c157c4bfbfeb6d5fe0b4
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-05-24 09:45:53 +0530

    initial commit ecent click

--- src/Home.js
+++ src/Home.js
@@ -4,10 +4,14 @@ function Home() {
   const handleClick = ()=>{
     console.log("hello Buddies")
   }
+  const handlClickAgain =(name)=>{
+    console.log("hello " +name)
+  }
   return (
     <div className='home'>
-      <h1>HomePage</h1>
+      <h1>HomePage</h1> 
       <button onClick={handleClick}>Click me</button>
+      <button onClick={()=>{handlClickAgain('Sau')}}>Click me Again</button>
     </div>
   )
 }
