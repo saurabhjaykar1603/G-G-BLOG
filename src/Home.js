49868b7550d86f2f8ab7ab4715f9b7e8d1fa4efe
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-05-24 09:33:54 +0530

    new feater - click event func added using handleClick

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 function Home() {
+  const handleClick = ()=>{
+    console.log("hello Buddies")
+  }
   return (
     <div className='home'>
       <h1>HomePage</h1>
+      <button onClick={handleClick}>Click me</button>
     </div>
   )
 }
